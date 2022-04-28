97b7b9f031e4771b41b2897648c500673b38ecf1
Stanislav Radevich <ganimed92@gmail.com>
2022-04-28 23:24:00 +0300

    add modules using

---
+++ modules_using/mymodule.py
@@ -0,0 +1,2 @@
+def my_func():
+    print("I'm from mymodule.py")
---
+++ modules_using/myprogram.py
@@ -0,0 +1,7 @@
+from main_package import main_script
+from main_package.sub_package import sub_script
+from mymodule import my_func
+
+main_script.report_main()
+sub_script.sub_report()
+my_func()
